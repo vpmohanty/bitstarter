be4e23bda6b8962662a26e825efbdf6f4a926fff
John Smith <example@stanford.edu>
2013-07-06 15:03:56 -0400

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,30 @@
 var express = require('express');
+var fs = require("fs");
+
+var filename = "index.html";
+var buffer;
+
+fs.exists(filename, function(exists) {
+    if (exists) {
+	fs.stat(filename, function(error, stats) {
+	    buffer = new Buffer(stats.size);
+
+	    buffer = fs.readFile(filename, function(err, data) {
+		if (err) throw err;
+		
+		console.log(data);
+	    });	   
+
+	    
+	});
+    }
+});
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send( buffer.toString() );
 });
 
 var port = process.env.PORT || 5000;
